957ffe56456b412fd0fae4007e7b7c2735c51c76
Foxhound-PC\Foxhound <andwamb@gmail.com>
2025-06-22 12:00:35 +0200

    missing included added

--- Source/GridMap/Private/GridMapStaticMeshActor.cpp
+++ Source/GridMap/Private/GridMapStaticMeshActor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GridMapStaticMeshActor.h"
+#include "Components/StaticMeshComponent.h"
 
 AGridMapStaticMeshActor::AGridMapStaticMeshActor(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
