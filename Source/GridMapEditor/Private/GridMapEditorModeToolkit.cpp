82422727290a48246ec5a58f0dc3e1be1ba0d825
Taron686 <155718583+Taron686@users.noreply.github.com>
2025-07-19 21:06:55 +0200

    Integrate grid map editor toolbar with level editor

--- Source/GridMapEditor/Private/GridMapEditorModeToolkit.cpp
+++ Source/GridMapEditor/Private/GridMapEditorModeToolkit.cpp
@@ -3,10 +3,13 @@
 #include "GridMapEditorModeToolkit.h"
 #include "GridMapEditorMode.h"
 #include "GridMapEditorToolkitWidget.h"
+#include "GridMapEditCommands.h"
 #include "Engine/Selection.h"
 #include "Widgets/Input/SButton.h"
 #include "Widgets/Text/STextBlock.h"
 #include "EditorModeManager.h"
+#include "Framework/MultiBox/MultiBoxBuilder.h"
+#include "LevelEditor.h"
 
 #define LOCTEXT_NAMESPACE "GridMapEditor"
 
@@ -16,8 +19,17 @@ FGridMapEditorModeToolkit::FGridMapEditorModeToolkit()
 
 void FGridMapEditorModeToolkit::Init(const TSharedPtr<IToolkitHost>& InitToolkitHost)
 {
-	ToolkitWidget = SNew(SGridMapEditorToolkitWidget);
-	FModeToolkit::Init(InitToolkitHost);
+        ToolkitWidget = SNew(SGridMapEditorToolkitWidget);
+        FModeToolkit::Init(InitToolkitHost);
+
+        if (FModuleManager::Get().IsModuleLoaded("LevelEditor"))
+        {
+                FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+
+                ToolBarExtender = MakeShareable(new FExtender);
+                ToolBarExtender->AddToolBarExtension("Settings", EExtensionHook::After, GetToolkitCommands(), FToolBarExtensionDelegate::CreateRaw(this, &FGridMapEditorModeToolkit::ExtendLevelEditorToolBar));
+                LevelEditorModule.GetToolBarExtensibilityManager()->AddExtender(ToolBarExtender);
+        }
 }
 
 FName FGridMapEditorModeToolkit::GetToolkitFName() const
@@ -37,7 +49,21 @@ class FEdMode* FGridMapEditorModeToolkit::GetEditorMode() const
 
 TSharedPtr<class SWidget> FGridMapEditorModeToolkit::GetInlineContent() const
 {
-	return ToolkitWidget;
+        return ToolkitWidget;
+}
+
+TSharedPtr<SWidget> FGridMapEditorModeToolkit::BuildToolBar()
+{
+        FToolBarBuilder ToolbarBuilder(GetToolkitCommands(), FMultiBoxCustomization::None);
+        ExtendLevelEditorToolBar(ToolbarBuilder);
+        return ToolbarBuilder.MakeWidget();
+}
+
+void FGridMapEditorModeToolkit::ExtendLevelEditorToolBar(FToolBarBuilder& ToolbarBuilder)
+{
+        ToolbarBuilder.AddToolBarButton(FGridMapEditCommands::Get().SetPaintTiles);
+        ToolbarBuilder.AddToolBarButton(FGridMapEditCommands::Get().SetSelectTiles);
+        ToolbarBuilder.AddToolBarButton(FGridMapEditCommands::Get().SetTileSettings);
 }
 
 #undef LOCTEXT_NAMESPACE
